c4614f0f93b290b048b61360c2eaeb0eb54b74ba
wuzibing <wuzibing@langmy.com>
2020-09-16 11:03:32 +0800

    update

---
+++ Excelize/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"github.com/wzbwzt/studyGo/Excelize/instore"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	instore.Init()
+	return
+
+	compile := regexp.MustCompile("\\d+\\.?\\d*|\\D*")
+	stuffone := "20140923001 | 深沟球轴承 | 6004 | 6004 | 10"
+	details := strings.Split(stuffone, "|")
+	for k,v:= range details{
+		space := strings.TrimSpace(v)
+		details[k]=space
+	}
+	fmt.Println(details)
+	if len(details) == 4 {
+		all := compile.FindAllStringSubmatch(details[3], 2)
+		fmt.Println(all)
+		//atoi, err := strconv.Atoi(all[0][0])
+		float, err := strconv.ParseFloat(all[0][0], 32)
+		if err != nil {
+			println(err)
+		}
+		fmt.Println(int64(float))
+		return
+		if all[0] != nil {
+			count, err := strconv.Atoi(all[0][0])
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(count)
+		}
+
+	}
+	if len(details)==5 {
+		all := compile.FindAllStringSubmatch(details[4], 2)
+		//count, err := strconv.ParseFloat(all[0][0], 32)
+		count, err := strconv.Atoi(all[0][0])
+		fmt.Println(all,len(all))
+		if err != nil {
+			all = compile.FindAllStringSubmatch(details[3], 2)
+			//atoi, err := strconv.ParseFloat(all[0][0], 32)
+			atoi, err := strconv.Atoi(all[0][0])
+			if err != nil {
+				println(err)
+			}
+			fmt.Println(atoi)
+		}else {
+			if len(all)>1 {
+				unit:= all[1][0]
+				fmt.Println(unit,count)
+			}
+			fmt.Println(123)
+
+		}
+	}
+}
