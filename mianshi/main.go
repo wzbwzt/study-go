d6b3f9485df7f51bb1a81a9be3b0009b7867d691
wuzibing <wuzibing@langmy.com>
2021-11-03 18:18:27 +0800

    update

--- mianshi/main.go
+++ mianshi/main.go
@@ -2,11 +2,21 @@ package main
 
 import (
 	"fmt"
+	"sort"
 	"strconv"
 )
 
 func main() {
+	fmt.Println(fmt.Sprintf("%.2f", float64(float64(20)/float64(50)*100)))
+
+	return
+	nums := []int{123, 2, 28, 18}
+	num := sort.IntSlice(nums)
+	num.Sort()
+	fmt.Println(nums)
+	return
 	var stock float64 = -140
 	res := strconv.FormatFloat(stock, 'e', 2, 64)
 	fmt.Println(res)
+
 }
