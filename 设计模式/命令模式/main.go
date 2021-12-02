a008ebd5dfaa8cc3f1291765492c4727fa45ba53
wuzibing <wuzibing@langmy.com>
2021-12-02 21:01:11 +0800

    update

--- 设计模式/命令模式/main.go
+++ 设计模式/命令模式/main.go
@@ -6,5 +6,85 @@ package main
 //3.方便记录执行过程，结合装饰器能更好更方便的扩展
 
 func main() {
+	logincmd := NewLoginCommand(&UserSvc{})
 
+	userInvoker := &Invoker{}
+	userInvoker.AddCommand(logincmd)
+	userInvoker.Do("joel", "123123")
+}
+
+//#####################################
+type UserSvc struct{}
+
+func (this *UserSvc) Login(name, passw string) error {
+	if name != "joel" || passw != "123123" {
+		println("login falied")
+		return nil
+	}
+	println("login success")
+	return nil
+}
+
+type ScoreSvc struct{}
+
+func (this *ScoreSvc) Score() error {
+	println("赠送积分成功")
+	return nil
+}
+
+//####################################
+//命令模式
+type ICommand interface {
+	Exec(args ...interface{}) error
+}
+
+//用户登陆命令
+type LoginCommand struct {
+	*UserSvc
+}
+
+func NewLoginCommand(svc *UserSvc) *LoginCommand {
+	return &LoginCommand{svc}
+}
+
+//TODO 可以添加装饰器，更好的扩展业务
+func (this *LoginCommand) Exec(args ...interface{}) error {
+	if len(args) != 2 {
+		panic("arg panic")
+	}
+	return this.Login(args[0].(string), args[1].(string))
+}
+
+//积分赠送命令
+type LoginCommand struct {
+	*UserSvc
+}
+
+func NewLoginCommand(svc *UserSvc) *LoginCommand {
+	return &LoginCommand{svc}
+}
+
+//TODO 可以添加装饰器，更好的扩展业务
+func (this *LoginCommand) Exec(args ...interface{}) error {
+	if len(args) != 2 {
+		panic("arg panic")
+	}
+	return this.Login(args[0].(string), args[1].(string))
+}
+
+//执行者:可以做队列等
+type Invoker struct {
+	cmds []ICommand
+}
+
+func (this *Invoker) Do(args ...interface{}) error {
+	for _, v := range this.cmds {
+		v.Exec(args...)
+
+	}
+	return nil
+}
+
+func (this *Invoker) AddCommand(cmds ...ICommand) {
+	this.cmds = append(this.cmds, cmds...)
 }
