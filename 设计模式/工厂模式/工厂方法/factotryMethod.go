38a6a4d5beca7ec4ca5614994521a483700df33a
wuzibing <wuzibing@langmy.com>
2021-12-02 19:03:57 +0800

    update

--- 设计模式/工厂模式/工厂方法/factotryMethod.go
+++ 设计模式/工厂模式/工厂方法/factotryMethod.go
@@ -3,39 +3,63 @@ package main
 //工厂方法：
 //多个工厂
 
+type ProductType int
+
+const (
+	COMPUTER ProductType = iota + 1
+	TSHIRT
+)
+
 //总工厂
 type IProductFactory interface {
-	CreateProduct()
+	CreateProduct(ProductType) IProduct
 }
 
 type IProduct interface {
 	GetInfo() string
 }
 
+//###########################################
+type Computer struct {
+}
+
+func (this *Computer) GetInfo() string {
+	return "computer"
+}
+
+type Tshirt struct {
+}
+
+func (this *Tshirt) GetInfo() string {
+	return "T-shirt"
+}
+
+//###########################################
+
 //技术类工厂
 type TechFactory struct{}
 
-func (*TechFactory) CreateProduct() IProduct {
-	return &TechFactory{}
-}
-
-func (*TechFactory) GetInfo() string {
-	return "computer"
+func (*TechFactory) CreateProduct(t ProductType) IProduct {
+	switch t {
+	case COMPUTER:
+		return &Computer{}
+	}
+	return nil
 }
 
 //日常类工厂
 type DailyFactory struct{}
 
-func (*DailyFactory) CreateProduct() IProduct {
-	return &DailyFactory{}
-}
-
-func (*DailyFactory) GetInfo() string {
-	return "T-shirt"
+func (*DailyFactory) CreateProduct(t ProductType) IProduct {
+	switch t {
+	case TSHIRT:
+		return &Tshirt{}
+	}
+	return nil
 }
 
 func main() {
-	println(new(TechFactory).CreateProduct().GetInfo())
-	println(new(DailyFactory).CreateProduct().GetInfo())
+	println(new(TechFactory).CreateProduct(COMPUTER).GetInfo())
+	println(new(DailyFactory).CreateProduct(TSHIRT).GetInfo())
 
 }
