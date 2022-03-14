8de954e96c39bd17fea1b4171768767a91a1f03d
wuzibing <wuzibing@langmy.com>
2022-03-14 12:14:24 +0800

    update

--- test/test.go
+++ test/test.go
@@ -1,14 +1,35 @@
 // +build ignore
+
 package main
 
 import (
 	"fmt"
-	"strings"
+	"sync"
 )
 
+//交替打印
 func main() {
-	s := "提交工单TS20222"
-	sp := strings.Split(s, "TS")
-	fmt.Printf("%v", s)
-	fmt.Printf("%v", sp)
+	//fmt.Println("123")
+	c1 := make(chan int)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		for i := 0; i < 10; i++ {
+			c1 <- 1
+			if i%2 == 0 {
+				fmt.Print(i)
+			}
+		}
+		wg.Done()
+	}()
+	go func() {
+		for i := 0; i < 10; i++ {
+			<-c1
+			if i%2 == 1 {
+				fmt.Print(i)
+			}
+		}
+		wg.Done()
+	}()
+	wg.Wait()
 }
