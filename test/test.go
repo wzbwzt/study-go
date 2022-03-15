2791d06127cb93ce2a395d1d6ac29b2a80f9a88d
wuzibing <wuzibing@langmy.com>
2022-03-15 19:18:26 +0800

    update

--- test/test.go
+++ test/test.go
@@ -5,10 +5,11 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 //交替打印
-func main() {
+func main1() {
 	//fmt.Println("123")
 	c1 := make(chan int)
 	wg := sync.WaitGroup{}
@@ -32,4 +33,18 @@ func main() {
 		wg.Done()
 	}()
 	wg.Wait()
+}
+
+func main() {
+	ch := make(chan struct{})
+	go func() {
+		fmt.Println("start working")
+		time.Sleep(time.Second * 1)
+		<-ch
+	}()
+
+	ch <- struct{}{}
+
+	fmt.Println("finished")
+	mapstructure.WeakDecode()
 }
