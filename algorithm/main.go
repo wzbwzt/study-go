0960a757850ab5ea2cf2b4ecaf2f4a9285b0f511
wzb <18721601308@163.com>
2022-03-21 10:19:43 +0800

    update

--- algorithm/main.go
+++ algorithm/main.go
@@ -3,6 +3,15 @@
 
 package main
 
+import "fmt"
+
+func main() {
+	// done, _ := bbb()
+	// done()
+	fmt.Println(numSubarrayProductLessThanK([]int{10, 5, 2, 6}, 100))
+}
+
+//##############################################################################
 func aaa() (done func(), err error) {
 	return func() {
 		print("aaa: done")
@@ -17,7 +26,26 @@ func bbb() (done func(), _ error) {
 	}, err
 }
 
-func main() {
-	done, _ := bbb()
-	done()
+//##############################################################################
+func numSubarrayProductLessThanK(nums []int, k int) int {
+	//思路1: 滑动窗口
+	//参数处理
+	if len(nums) == 0 || k <= 0 {
+		return 0
+	}
+
+	//滑动窗口
+	res := 0
+	left, right, product := 0, 0, 1
+	for ; right < len(nums); right++ {
+		product *= nums[right]
+		for left <= right && product >= k {
+			product /= nums[left]
+			left++
+		}
+		if left <= right {
+			res += right - left + 1
+		}
+	}
+	return res
 }
